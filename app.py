e8a6bfac2a3fef3d50dfd993093613f73d6004a9
AntonyIS <antonyshikubu@gmail.com>
2020-03-19 07:23:07 +0300

    Added the base template using jinja2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -7,42 +7,48 @@ app = Flask(__name__)
 # http://127.1.1.0:5000
 @app.route('/')
 def index():
-	return "Hello world"
+	title  = "Covid-19 | Home "
+	return render_template('index.html', title = title)
 
 # /signup
 # http://127.1.1.0:5000/signup
 @app.route('/signup')
 def signup():
-	return "Signup page"
+	title = "Covid-19 |Signup "
+	return render_template('signup.html',title = title)
 
 # /login
 # http://127.1.1.0:5000/login
 @app.route('/login')
 def login():
-	return "Login page"
+	title = "Covid-19 | Login "
+	return render_template('login.html',title = title)
 
 # /account
 # http://127.1.1.0:5000/login
 @app.route('/account')
 def account():
-	return "account page"
+	title = "Covid-19 | Account "
+	return render_template('account.html',title = title)
 
 # /posts/id
 # http://127.1.1.0:5000/posts/121231232
 @app.route('/posts/<int:post_id>')
-def posts(post_id):
-	return "Post detail page page"
+def post_detail(post_id):
+	title = "Covid-19 | Post Detail "
+	return render_template('post_detail.html',title = title)
 
 # /posts/id
 # http://127.1.1.0:5000/update/posts/121231232
 @app.route('/posts/update/<int:post_id>')
-def posts(post_id):
+def post_update(post_id):
+
 	return "Post update route"
 
 # /posts/id
 # http://127.1.1.0:5000/posts/delete/121231232
-@app.route('/posts/update/<int:post_id>')
-def posts(post_id):
+@app.route('/posts/delete/<int:post_id>')
+def post_delete(post_id):
 	return "Post delete route"
 
 
